491778648c95298187bd2306f88f849b185aa204
sanya <alexgitlinux@gmail.com>
2025-06-27 15:54:58 +0700

    Logging has deleted from controller.

--- src/Controller/PropertiesController.php
+++ src/Controller/PropertiesController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Attributes\ValidatePropertiesDto;
 use App\Dto\PropertiesDto;
 use App\Service\PropertiesService;
-use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,20 +20,11 @@ final class PropertiesController extends AbstractController
     #[Route(path: '/properties/store', name: 'properties', methods: ['POST'])]
     #[ValidatePropertiesDto]
     public function store(
-        LoggerInterface     $loggerInterface,
         Request             $request,
         SerializerInterface $serializer,
         PropertiesService   $propertiesService,
     ): JsonResponse
     {
-        $loggerInterface->info(
-            "Get request for store new Property.",
-            json_decode(
-                $request->getContent(),
-                true
-            ),
-        );
-
         /** @var PropertiesDto $propertiesDto */
         $propertiesDto = $serializer->deserialize(
             $request->getContent(),
